971828ab4562e549a42476134a20447d7d847782
Denis Sowa <denis@sowa.info>
2022-04-07 20:22:22 +0200

    Combined the Win, Mac & Linux version into one tpp file. Should be easier to download and install.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,8 @@
+## Version 1.0.3
+- Combined the Win, Mac & Linux version into one tpp file. Should be easier to download and install.
 ## Version 1.0.2
-
 - Include .NET runtime. Installation is not necessary any longer.
-
 ## Version 1.0.1
-
 - Fix missing icon.
-
 ## Version 1.0.0
-
 - First Version.
--- build.sh
+++ build.sh
@@ -4,20 +4,21 @@ dotnet publish -c Release -r osx-x64 -p:PublishReadyToRun=true --self-contained
 dotnet publish -c Release -r win-x64 -p:PublishReadyToRun=true --self-contained
 dotnet publish -c Release -r linux-x64 -p:PublishReadyToRun=true --self-contained
 
-mkdir "Release"
-
 cd "bin/"
 
-cp -R "Release/net6.0/osx-x64/publish" "TouchPortalMuteMePlugin"
-zip -q -r "../Release/OSX_Touch_Portal_MuteMe_Plugin.tpp" "TouchPortalMuteMePlugin" -x "*.DS_Store"
-rm -R "TouchPortalMuteMePlugin"
+#mkdir -p "TouchPortalMuteMePlugin"
+mkdir -p "TouchPortalMuteMePlugin/osx"
+mkdir -p "TouchPortalMuteMePlugin/win"
+mkdir -p "TouchPortalMuteMePlugin/linux"
 
-cp -R "Release/net6.0/win-x64/publish" "TouchPortalMuteMePlugin"
-zip -q -r "../Release/WIN_Touch_Portal_MuteMe_Plugin.tpp" "TouchPortalMuteMePlugin" -x "*.DS_Store"
-rm -R "TouchPortalMuteMePlugin"
+cp -R "Release/net6.0/osx-x64/publish/" "TouchPortalMuteMePlugin/osx/"
+cp -R "Release/net6.0/win-x64/publish/" "TouchPortalMuteMePlugin/win/"
+cp -R "Release/net6.0/linux-x64/publish/" "TouchPortalMuteMePlugin/linux/"
 
-cp -R "Release/net6.0/linux-x64/publish" "TouchPortalMuteMePlugin"
-zip -q -r "../Release/LINUX_Touch_Portal_MuteMe_Plugin.tpp" "TouchPortalMuteMePlugin" -x "*.DS_Store"
+cp "Release/net6.0/osx-x64/publish/icon24.png" "TouchPortalMuteMePlugin/"
+cp "Release/net6.0/osx-x64/publish/entry.tp" "TouchPortalMuteMePlugin/"
+
+zip -q -r "../Touch_Portal_MuteMe_Plugin.tpp" "TouchPortalMuteMePlugin" -x "*.DS_Store"
 rm -R "TouchPortalMuteMePlugin"
 
 cd ".."
